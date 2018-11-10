8dc3ecfb3f64cbc1e6c1908da82860b07e5aad6e
VenkataSambasivaRao <sambakesanam@gmail.com>
2018-11-10 19:10:23 +0400

    Bot Messages

--- Dialogs/EchoDialog.cs
+++ Dialogs/EchoDialog.cs
@@ -4,7 +4,7 @@ using System.Threading.Tasks;
 using Microsoft.Bot.Connector;
 using Microsoft.Bot.Builder.Dialogs;
 using System.Net.Http;
-
+using Newtonsoft.Json;
 
 namespace Microsoft.Bot.Sample.SimpleEchoBot
 {
@@ -12,6 +12,17 @@ namespace Microsoft.Bot.Sample.SimpleEchoBot
     public class EchoDialog : IDialog<object>
     {
         protected int count = 1;
+        static string TRANSFER_MESSAGE = "transfer to ";
+
+        // Live Assist custom channel data.
+        public class LiveAssistChannelData
+        {
+            [JsonProperty("type", NullValueHandling = NullValueHandling.Ignore)]
+            public string Type { get; set; }
+
+            [JsonProperty("skill", NullValueHandling = NullValueHandling.Ignore)]
+            public string Skill { get; set; }
+        }
 
         public async Task StartAsync(IDialogContext context)
         {
@@ -22,7 +33,58 @@ namespace Microsoft.Bot.Sample.SimpleEchoBot
         {
             var message = await argument;
 
-            if (message.Text == "reset")
+            if (message.ChannelId == "directline")
+            {
+                var laChannelData = message.GetChannelData<LiveAssistChannelData>();
+
+                switch (laChannelData.Type)
+                {
+                    case "visitorContextData":
+                        //process context data if required. This is the first message received so say hello.
+                        await context.PostAsync("Hi, I am an echo bot and will repeat everything you said.");
+                        break;
+
+                    case "systemMessage":
+                        //react to system messages if required
+                        break;
+
+                    case "transferFailed":
+                        //react to transfer failures if required
+                        break;
+
+                    case "otherAgentMessage":
+                        //react to messages from a supervisor if required
+                        break;
+
+                    case "visitorMessage":
+                        // Check for transfer message
+
+                        if (message.Text.StartsWith(TRANSFER_MESSAGE))
+                        {
+                            var reply = context.MakeMessage();
+                            var transferTo = message.Text.Substring(TRANSFER_MESSAGE.Length);
+
+                            reply.ChannelData = new LiveAssistChannelData()
+                            {
+                                Type = "transfer",
+                                Skill = "BotEscalation"
+                            };
+
+                            await context.PostAsync(reply);
+                        }
+                        else
+                        {
+                            await context.PostAsync("You said: " + message.Text);
+                        }
+                        break;
+
+                    default:
+                        await context.PostAsync("This is not a Live Assist message " + laChannelData.Type);
+                        break;
+                }
+            }
+
+            else if (message.Text == "reset")
             {
                 PromptDialog.Confirm(
                     context,
