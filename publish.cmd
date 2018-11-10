5910f8115f2f6101d56afbeab095579874e50201
VenkataSambasivaRao <sambakesanam@gmail.com>
2018-11-10 19:04:32 +0400

    Add project files.

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=botwasl-Web-Deploy.pubxml -p:Password=pDM90W97WaaJtnPMADDNSRXq0dPTkXKp7TxnDJ1hvPDZHrHKP4ejGu9muc3x
+
