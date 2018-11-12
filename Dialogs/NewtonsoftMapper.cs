801e5705e517f4cfedeacfada165b511f8f5014d
VenkataSambasivaRao <sambakesanam@gmail.com>
2018-11-12 13:08:39 +0400

    Live Assist With Chat History

---
+++ Dialogs/NewtonsoftMapper.cs
@@ -0,0 +1,28 @@
+﻿using Jose;
+using Newtonsoft.Json;
+
+namespace SimpleEchoBot.Dialogs
+{
+    public class NewtonsoftMapper : IJsonMapper
+    {
+        public string Serialize(object obj)
+        {
+            var settings = new JsonSerializerSettings
+            {
+                NullValueHandling = NullValueHandling.Ignore
+            };
+
+            return JsonConvert.SerializeObject(obj, Formatting.Indented, settings);
+        }
+
+        public T Parse<T>(string json)
+        {
+            var settings = new JsonSerializerSettings
+            {
+                NullValueHandling = NullValueHandling.Ignore
+            };
+
+            return JsonConvert.DeserializeObject<T>(json, settings);
+        }
+    }
+}
